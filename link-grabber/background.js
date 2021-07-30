9135bf78e3f474501b9d6ef9c87fc396a5fd79a3
Administrator <Administrator@DESKTOPSERVER>
2021-07-30 10:21:59 +1000

    removed console.log

--- link-grabber/background.js
+++ link-grabber/background.js
@@ -2,16 +2,11 @@ var bwIcon = "16-bw.png";
 var normalIcons = {"16": "16.png", "48": "48.png", "128": "128.png"};
 
 function sendDownload(downloadItem) {
-  console.log(downloadItem.finalUrl);
-  console.log(downloadItem.startTime);
-
   chrome.storage.local.get(["enabled"], function(result) {
-    console.log(result.hasOwnProperty("enabled"));
     var isEnabled = true;
     if (result.hasOwnProperty("enabled")) {
       isEnabled = result.enabled;
     }
-    console.log(isEnabled);
     if (isEnabled == false) return;
     var startTime = Date.parse(downloadItem.startTime);
     var now = Date.now();
@@ -19,7 +14,6 @@ function sendDownload(downloadItem) {
       var encodedUrl = btoa(downloadItem.finalUrl);
       fetch("http://localhost:13000/" + encodedUrl)
         .then(function(response) {
-          console.log(response.status);
           if (response.status == 403) {
             chrome.downloads.cancel(downloadItem.id);
           }
@@ -33,18 +27,14 @@ function sendDownload(downloadItem) {
 
 function setIconStatus(tab)
 {
-  console.log("icon clicked.");
   chrome.storage.local.get(["enabled"], function(result) {
-    console.log("Value currently is " + result.enabled);
     if (result.hasOwnProperty("enabled")) {
       chrome.storage.local.set({enabled: !result.enabled}, function() {
-        console.log("Value is set to " + !result.enabled);
         setIconStyle(!result.enabled);
       });
     }
     else {
       chrome.storage.local.set({enabled: false}, function() {
-        console.log("Value is set to false.");
         setIconStyle(false);
       });
     }
@@ -57,7 +47,6 @@ function syncIconStatus(tab) {
     if (result.hasOwnProperty("enabled")) {
       isEnabled = result.enabled;
     }
-    console.log(isEnabled);
     setIconStyle(isEnabled);
   });
 }
