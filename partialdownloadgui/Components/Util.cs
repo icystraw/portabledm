b13dc2aeefbae54e81273e5b33f863bb5582fd99
Administrator <Administrator@DESKTOPSERVER>
2021-08-09 17:53:40 +1000

    sync

--- partialdownloadgui/Components/Util.cs
+++ partialdownloadgui/Components/Util.cs
@@ -205,7 +205,7 @@ namespace partialdownloadgui.Components
                 if (ds.HttpStatusCode != HttpStatusCode.OK && ds.HttpStatusCode != HttpStatusCode.PartialContent)
                 {
                     response.Dispose();
-                    ds.Error = "HTTP status is not 200 or 206.";
+                    ds.Error = "HTTP status is not 200 or 206. Check your link and try again.";
                     ds.DownloadStatus = DownloadStatus.DownloadError;
                     return;
                 }
