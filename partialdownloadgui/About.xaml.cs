f40acb6347531b0ced2c1e3a87f7202a7ec3e037
Administrator <Administrator@DESKTOPSERVER>
2021-08-23 14:33:36 +1000

    about done.

--- partialdownloadgui/About.xaml.cs
+++ partialdownloadgui/About.xaml.cs
@@ -1,16 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace partialdownloadgui
 {
@@ -22,6 +10,11 @@ namespace partialdownloadgui
         public About()
         {
             InitializeComponent();
+        }
+
+        private void Window_MouseLeftButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            Close();
         }
     }
 }
